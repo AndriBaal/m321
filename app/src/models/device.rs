f3852a05afbc2443b3934fc90ae574e5c6373824
Sullivan Renner <130152349+srenner06@users.noreply.github.com>
2025-05-23 17:17:05 +0200

    Delete app/src/models/device.rs

--- app/src/models/device.rs
+++
@@ -1,8 +0,0 @@
-use serde::{Deserialize, Serialize};
-use uuid::Uuid;
-
-#[derive(Debug, Default, Serialize, Deserialize)]
-pub struct Device {
-    #[serde(rename = "_id", skip_serializing)]
-    pub id: Option<Uuid>
-}
